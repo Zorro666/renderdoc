872ddb87055490a29ad3f1a5efd3521c1001a2e2
baldurk <baldurk@baldurk.org>
2018-09-02 13:17:45 +0100

    Implement hooking support on apple
    * This assumes it will be used in concert with dyld interposing so it doesn't
      actually do any hooking apart from intercepting dlopen/dlsym to substitute any
      dynamic access to hooked functions. Otherwise it just fulfills the contract by
      calling callbacks and populating original function pointers

--- renderdoc/os/posix/apple/apple_hook.cpp
+++ renderdoc/os/posix/apple/apple_hook.cpp
@@ -125,7 +125,7 @@ void LibraryHooks::EndHookRegistration()
     if(hook.orig && *hook.orig == NULL)
     {
       *hook.orig = dlsym(RTLD_NEXT, hook.function.c_str());
-      RDCASSERT(*hook.orig != hook.hook, hook.function);
+      RDCASSERT(*hook.orig, hook.hook, hook.function);
     }
   }
 }
