410f49f7312920f4cb04820267fa3fc578678019
Jake Turner <jake@evansturner.co.uk>
2024-12-19 12:42:07 +0000

    Show source debug location in disassembly comments

--- renderdoc/driver/shaders/dxil/dxil_disassemble.cpp
+++ renderdoc/driver/shaders/dxil/dxil_disassemble.cpp
@@ -3359,6 +3359,8 @@ void Program::MakeRDDisassemblyString(const DXBC::Reflection *reflection)
                                            DXBC::BasicDemangle(func.blocks[curBlock]->name).c_str(),
                                            func.blocks[curBlock]->id);
       DisassemblyAddNewLine(1);
+      uint32_t currentDebugLoc = ~0U;
+      const Metadata *lastScope = NULL;
 
       DXIL::ControlFlow controlFlow;
 
@@ -5163,6 +5165,36 @@ void Program::MakeRDDisassemblyString(const DXBC::Reflection *reflection)
             break;
           }
         }
+        uint32_t dbgLoc = ShouldIgnoreSourceMapping(inst) ? ~0U : inst.debugLoc;
+        if(dbgLoc != ~0U)
+        {
+          // if(dbgLoc != currentDebugLoc)
+          {
+            const DebugLocation &debugLoc = m_DebugLocations[dbgLoc];
+            if(!commentStr.empty())
+              commentStr += ", ";
+            const Metadata *scope = debugLoc.scope;
+            if(scope)
+            {
+              const DIBase *const dwarfInfo = scope->dwarf;
+              if(dwarfInfo)
+              {
+                commentStr += " ; ";
+                commentStr += " " + ToStr(dwarfInfo->type);
+                commentStr += " ";
+                commentStr += GetFunctionScopeName(dwarfInfo);
+                commentStr += " ";
+                rdcstr shaderFilePath =
+                    standardise_directory_separator(GetDebugScopeFilePath(dwarfInfo));
+                commentStr += get_basename(shaderFilePath);
+              }
+              lastScope = scope;
+            }
+            commentStr += StringFormat::Fmt(" line:%llu col:%llu", debugLoc.line, debugLoc.col);
+            currentDebugLoc = dbgLoc;
+          }
+        }
+
         if(!resultIdStr.empty())
           lineStr = resultTypeStr + resultIdStr + " = " + lineStr;
 
