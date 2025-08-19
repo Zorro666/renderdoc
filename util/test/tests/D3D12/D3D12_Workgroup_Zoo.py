4ba3f9992680b3d34e34be0bf48e8c98ba660504
Jake Turner <jake@evansturner.co.uk>
2025-08-19 12:31:50 +0100

    HACK Python check five shader debug traces for determinism
    Compare single threaded traces against multi-threaded traces
    
    Iter_Test,
    D3D12_Workgroup_Zoo, D3D12_Subgroup_Zoo, D3D12_Shader_Debug_Zoo,
    VK_Workgroup_Zoo, VK_Subgroup_Zoo, VK_Shader_Debug_Zoo

--- util/test/tests/D3D12/D3D12_Workgroup_Zoo.py
+++ util/test/tests/D3D12/D3D12_Workgroup_Zoo.py
@@ -1,5 +1,7 @@
 import rdtest
+import renderdoc as rd
 
 class D3D12_Workgroup_Zoo(rdtest.Workgroup_Zoo):
     demos_test_name = 'D3D12_Workgroup_Zoo'
     internal = False
+    mtOption = rd.SetConfigSetting("D3D12_DXILShaderDebugger_EnableMT")
