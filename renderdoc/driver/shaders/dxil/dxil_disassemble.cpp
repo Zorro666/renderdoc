647af62d5b54a73c6b908f7b7e93226d480ee166
Jake Turner <jake@evansturner.co.uk>
2025-08-21 14:03:22 +0100

    HACK DXBC UNIT TEST RUNS CONTROL FLOW CONSTRUCT

--- renderdoc/driver/shaders/dxil/dxil_disassemble.cpp
+++ renderdoc/driver/shaders/dxil/dxil_disassemble.cpp
@@ -30,6 +30,7 @@
 #include "strings/string_utils.h"
 #include "dxil_bytecode.h"
 #include "dxil_common.h"
+#include "dxil_controlflow.h"
 #include "dxil_debuginfo.h"
 
 #if ENABLED(DXC_COMPATIBLE_DISASM) && ENABLED(RDOC_RELEASE)
@@ -3359,6 +3360,21 @@ void Program::MakeRDDisassemblyString(const DXBC::Reflection *reflection)
                                            func.blocks[curBlock]->id);
       DisassemblyAddNewLine(1);
 
+      DXIL::ControlFlow controlFlow;
+
+      // Find the uniform control blocks in the function
+      rdcarray<rdcpair<uint32_t, uint32_t>> links;
+      for(const Block *block : func.blocks)
+      {
+        for(const Block *pred : block->preds)
+        {
+          uint32_t from = pred->id;
+          uint32_t to = block->id;
+          links.push_back({from, to});
+        }
+      }
+      controlFlow.Construct(links);
+
       for(size_t funcIdx = 0; funcIdx < func.instructions.size(); funcIdx++)
       {
         Instruction &inst = *func.instructions[funcIdx];
@@ -6398,5 +6414,4 @@ SourceMappingInfo Program::ParseDbgOpDeclare(const DXIL::Instruction &inst) cons
 
   return ret;
 }
-
 };    // namespace DXIL
