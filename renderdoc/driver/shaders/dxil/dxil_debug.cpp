1f1a35cb9e254d5f12064db10f26e6fe6d234772
Jake Turner <jake@evansturner.co.uk>
2025-09-26 12:56:27 +0100

    JAKE DXIL DEBUGGER HACK SIMULATE LANES IN A RANDOM ORDER

--- renderdoc/driver/shaders/dxil/dxil_debug.cpp
+++ renderdoc/driver/shaders/dxil/dxil_debug.cpp
@@ -10201,6 +10201,7 @@ rdcarray<ShaderDebugState> Debugger::ContinueDebug()
       }
 
       // step all threads in the tangle
+      rdcarray<uint32_t> lanesToSimulate;
       for(const ThreadReference &ref : threadRefs)
       {
         const uint32_t threadId = ref.id;
@@ -10217,8 +10218,14 @@ rdcarray<ShaderDebugState> Debugger::ContinueDebug()
           hasDebugState = true;
 
         thread.SetActiveMask(activeMask);
-        QueueJob(lane);
+        lanesToSimulate.push_back(lane);
       }
+      while(!lanesToSimulate.empty())
+      {
+        uint32_t i = rand() % lanesToSimulate.size();
+        QueueJob(lanesToSimulate[i]);
+        lanesToSimulate.erase(i);
+      };
     }
 
     do
